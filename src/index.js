f1fc4521f0372fc1c33308ba1bcc6938218cfcf0
marriet-aho <marrietaho2020@gmail.com>
2024-11-25 14:49:21 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+alert("Hello World")
+alert("Marriet Aho")
+
+
+let Name=prompt("what is your name?")
+alert(Name + "it is!")
+alert()
+
+// variable naming conventions
+let friend = prompt("what is your friend's name?");
+let age = prompt("what is your friend's age?");
+let favColor = prompt("what is your friend's favorite color?");
+
+alert("My beautiful friend is "  + friend +  " she is "  + age +  " and her favorite color "  +  favColor )
+alert(`My beatiful friend's name is ${friend}. She is ${age} years old and her favorite color is ${favColor} 😂😂👩‍💻`)
+
+// My beautiful friend's name is   .she is   years of age and her favrite color is Orange.
+
